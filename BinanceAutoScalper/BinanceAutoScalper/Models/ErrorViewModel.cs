55aebbe0db65ddc1207b538db462fc780c55c2b5
André Lashley <andre.lashley@gmail.com>
2023-01-24 17:44:45 -0800

    Add initial project

---
+++ BinanceAutoScalper/BinanceAutoScalper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BinanceAutoScalper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
