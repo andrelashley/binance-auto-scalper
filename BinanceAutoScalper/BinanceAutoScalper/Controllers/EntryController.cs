e95b5b8a0501940231b2912943d4b2fce22a9f40
André Lashley <andre.lashley@gmail.com>
2023-01-24 17:56:12 -0800

    Add a controller and view for daily entries

---
+++ BinanceAutoScalper/BinanceAutoScalper/Controllers/EntryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BinanceAutoScalper.Controllers
+{
+    public class EntryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
